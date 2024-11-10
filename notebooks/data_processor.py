3ecd7a17fc1ecfe2a3de3e76b2d132ca8bfcac1e
Matthew Graca <matthew.graca@gmail.com>
2024-11-09 16:45:19 -0800

    have functions do encoding as well, pulled df argument into constructor

--- notebooks/data_processor.py
+++ notebooks/data_processor.py
@@ -3,14 +3,16 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import OrdinalEncoder
 
 class DataProcessor:
-    def __init__(self):
+    def __init__(self, df):
+        self.df = df
+
         # categorical features that present as numerical
         self.extracted_cat_features = [
             'MSSubClass', 'YearBuilt', 'YearRemodAdd', 'BsmtFullBath', 
             'BsmtHalfBath', 'GarageYrBlt', 'MoSold', 'YrSold'
         ]
 
-        # ordinal features
+        # ordinal features that present as categorical
         self.extracted_ord_features = [
             'LandSlope', 'BsmtQual', 'BsmtCond', 'BsmtFinType1', 'BsmtFinType2', 
             'HeatingQC', 'Electrical', 'KitchenQual', 'Functional', 'FireplaceQu', 
@@ -21,85 +23,73 @@ class DataProcessor:
     Process the original data, isolating the numerical features.
     - The 'Id' feature is automatically dropped.
     - NaN values imputed with 0
-
-    Params:
-        df: the dataframe containing the **original** data
+    - Ordinal features encoded and added
+    - Target column placed at the end of the dataframe
 
     Returns:
         A dataframe with only the numerical features
     '''
-    def numerical_data(self, df):
-        num_df = df.select_dtypes(exclude='object')
+    def numerical_data(self):
+        # set up numerical dataframe
+        num_df = self.df.select_dtypes(exclude='object')
         num_df.drop('Id', axis=1, inplace=True)
         num_df.drop(self.extracted_cat_features, axis=1, inplace=True)
         num_df.fillna(0, inplace=True)
+        target = num_df['SalePrice']
+        num_df.drop('SalePrice', axis=1, inplace=True)
+        
+        # encode ordinal features
+        ord_df = self.df[self.extracted_ord_features]
+        enc = OrdinalEncoder(encoded_missing_value=-1)
+        enc.set_output(transform='pandas')
+        encoded_ord_df = enc.fit_transform(ord_df)
 
-        return num_df
+        return pd.concat([num_df, encoded_ord_df, target], axis=1) 
     
     '''
     Process the original data, isolating the categorical features.
     - This removes the ordinal features by default 
 
     Params:
-        df: The dataframe containing the **original** data
         drop_ordinal: Determines if the ordinal data should be dropped. 
             True by default.
 
     Returns:
         A dataframe with only the categorical features
     '''
-    def categorical_data(self, df, drop_ordinal=True):
-        cat_df = df.select_dtypes(include='object')
-        cat_df = pd.concat([cat_df, df[self.extracted_cat_features]], axis=1)
-        if drop_ordinal:
-            cat_df.drop(self.extracted_ord_features, axis=1, inplace=True)
-        return cat_df
+    def categorical_data(self):
+        cat_df = self.df.select_dtypes(include='object')
 
-    '''
-    Process the original data, isolating the ordinal features.
-    - If you want only numerical and categorical features, perform ordinal 
-        encoding and concatenate to the numerical dataframe.
-
-    Params:
-        df: the dataframe containing the **original** data
-
-    Returns:
-        A dataframe with only the ordinal features
-    '''
-    def ordinal_data(self, df):
-        return df[self.extracted_ord_features]
-
-    '''
-    Process the original training data, into one more usable by regression.
-    - All numerical features preserved with NaNs converted to 0.
-    - All categorical features one-hot encoded.
-    - Categorical features with natural ordering encoded with ordinal encoding.
-
-    Params
-        df: the dataframe containing the **original** training data
-
-    Returns
-        A dataframe with the above operations performed, usuable for regression.
-    '''
-    def encode_data(self, df):
-        # separate into numerical and categorical
-        num_df = self.numerical_data(df) 
-        cat_df = self.categorical_data(df)
-        ord_df = self.ordinal_data(df)
+        # add categorical features that are not 'object' type
+        # drop features that are 'object', but will be converted to numeric
+        cat_df = pd.concat([cat_df, self.df[self.extracted_cat_features]], axis=1)
+        cat_df.drop(self.extracted_ord_features, axis=1, inplace=True)
 
         # encode categorical features
         enc = OneHotEncoder(sparse_output=False)
         enc.set_output(transform='pandas')
         encoded_cat_df = enc.fit_transform(cat_df)
 
-        # encode ordinal features
-        enc = OrdinalEncoder(encoded_missing_value=-1)
-        enc.set_output(transform='pandas')
-        encoded_ord_df = enc.fit_transform(ord_df)
+        return encoded_cat_df
+
+    '''
+    Process the original training data, into one more usable by regression.
+    - All numerical features preserved with NaNs converted to 0.
+    - All ordinal features encoded.
+    - All categorical features one-hot encoded.
+    - Categorical features with natural ordering encoded with ordinal encoding.
+
+    Returns
+        A dataframe with the above operations performed, usuable for regression.
+    '''
+    def complete_data(self):
+        # separate into numerical and categorical
+        num_df = self.numerical_data() 
+        cat_df = self.categorical_data()
 
         # merge all the dfs
         target_col = num_df["SalePrice"]
         num_df.drop("SalePrice", axis=1, inplace=True)
-        train_df = pd.concat([num_df, encoded_cat_df, encoded_ord_df, target_col], axis=1)
 
-        return train_df
+        return pd.concat([num_df, cat_df, target_col], axis=1)
+
